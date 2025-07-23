e466d8cfb9c3590c1d0c91550d4e1273be55437b
sakshyampudasaini <pudasainisakshyam@gmail.com>
2025-07-24 00:44:45 +0545

    TEMPORARY: Teacher-approved ALLOWED_HOSTS setting

--- core/settings.py
+++ core/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = "django-insecure-oq#!18k_%ibaue6b7k^7cqv)h^(=-yvz&h59)9e8^g)=%ejq3#
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = [
-    'sds-6hh7.onrender.com',  # Your Render URL
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']
 DEBUG = False  # Must be False for production!
 
 
