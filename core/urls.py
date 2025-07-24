41bbce43beb543bd803357e5f29101c758ccb46e
sakshyampudasaini <pudasainisakshyam@gmail.com>
2025-07-24 10:33:46 +0545

    FIX: Add Render domain to ALLOWED_HOSTS

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from core.views import home
+from django import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('', home, name='home'),  # Homepage
+        path('', views.home, name='home'),  # Add a root route
+
     path('students/', include('students.urls')),
 ]
